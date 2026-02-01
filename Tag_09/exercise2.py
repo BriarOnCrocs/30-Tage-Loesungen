7f6783c8110defffa661f3a522051f4c668560e4
BriarOnCrocs <leon.204kuehne@gmail.com>
2026-02-01 17:51:57 +0100

    Tag 9 des Kurses

--- Tag_09/exercise2.py
+++ Tag_09/exercise2.py
@@ -2,14 +2,14 @@
 # 1
 score = int(input("Please enter your score: "))
 
-def grades(score):
-    if 90 <= score <= 100:
+def grades(fscore):
+    if 90 <= fscore <= 100:
         return "A"
-    elif 80 <= score <= 89:
+    elif 80 <= fscore <= 89:
         return "B"
-    elif 70 <= score <= 79:
+    elif 70 <= fscore <= 79:
         return "C"
-    elif 60 <= score <= 69:
+    elif 60 <= fscore <= 69:
         return "D"
     else:
         return "F"
@@ -20,14 +20,14 @@ print(f"The grade for the score {score} ist {grade}")
 # 2
 month = str(input("Enter a month to see the season: ")).lower()
 
-def seasons(month):
-    if month in ("september", "october", "november"):
+def seasons(fmonth):
+    if fmonth in ("september", "october", "november"):
         return "Autumn"
-    elif month in ("december", "january", "february"):
+    elif fmonth in ("december", "january", "february"):
         return "Winter"
-    elif month in ("march", "april", "may"):
+    elif fmonth in ("march", "april", "may"):
         return "Spring"
-    elif month in ("june", "july", "august"):
+    elif fmonth in ("june", "july", "august"):
         return "Summer"
     else:
         return f"error, no valid month"
@@ -38,12 +38,12 @@ print(f"The season for \"{month.capitalize()}\" is {season}.")
 # 3
 new_fruit = str(input("Enter a new fruit!: "))
 fruits_list = ['banana', 'orange', 'mango', 'lemon']
-def fruits(new_fruit):
-    if new_fruit not in fruits_list:
-        fruits_list.append(new_fruit)
+def fruits(fnew_fruit):
+    if fnew_fruit not in fruits_list:
+        fruits_list.append(fnew_fruit)
         return fruits_list
     else:
-        return f"{new_fruit.capitalize()} is already in the list: {fruits_list}"
+        return f"{fnew_fruit.capitalize()} is already in the list: {fruits_list}"
 
 add_to_list = fruits(new_fruit)
 print(f"New list: {add_to_list}.")
